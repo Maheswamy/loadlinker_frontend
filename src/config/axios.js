365a71592922c7f9a2906ce22a6d0b5580132d5d
Maheswamy <mahendragowdas1997@gmail.com>
2023-11-23 18:20:10 +0530

    base url updated

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:3080",
+  // baseURL: "http://localhost:3080",
+  baseURL: "https://loadlinker-backend.onrender.com",
 });
